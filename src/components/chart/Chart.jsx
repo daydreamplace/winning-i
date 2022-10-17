72931a8550e6db89055265d39a380bbff6fb9ed3
youbinpark <you8inpark@gmail.com>
2022-10-18 06:13:53 +0900

    Add: Chart 레이아웃 구현

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,15 @@
+import Vistor from "./components/Vistor";
+import AgeGroup from "./components/AgeGroup";
+import Post from "./components/Post";
+
+const Chart = () => {
+  return (
+    <>
+      <Vistor />
+      <AgeGroup />
+      <Post />
+    </>
+  );
+};
+
+export default Chart;
---
+++ src/components/chart/components/Post.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Chart from "react-apexcharts";
+import { COLOR_2 } from "./constants/ChartConstant";
+
+const Post = () => {
+  return (
+    <Chart
+      options={{
+        chart: {
+          type: "line",
+          zoom: {
+            enabled: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "smooth",
+          width: 3,
+        },
+        colors: [COLOR_2],
+        xaxis: {
+          categories: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+          ],
+        },
+      }}
+      series={[
+        {
+          name: "Desktops",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+        },
+      ]}
+      height={300}
+    />
+  );
+};
+
+export default Post;
