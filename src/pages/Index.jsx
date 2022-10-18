ed52461a9099b3a8e1b3a84029c42e6bc7490d70
youbinpark <you8inpark@gmail.com>
2022-10-18 15:26:26 +0900

    Add: Main page 레이아웃 구현

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import { Layout, Menu } from "antd";
 import Chart from "../components/chart/Chart";
+import { BarChartOutlined, UserOutlined } from "@ant-design/icons";
+import { BsFileEarmarkPost } from "react-icons/bs";
+import styled from "styled-components";
+
+const { Header, Content, Sider } = Layout;
 
 const Main = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
+
+  function getItem(label, key, icon, children, link) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      ...(link && {
+        onClick: () => {
+          navigate(link);
+        },
+      }),
+    };
+  }
+
+  const items = [
+    getItem("CHART", "1", <BarChartOutlined />),
+    getItem("BOARD", "2", <BsFileEarmarkPost />),
+    getItem("USER SETTING", "3", <UserOutlined />),
+  ];
+
   return (
-    <>
-      <Chart />
-    </>
+    <MainPage>
+      <Layout
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0,
+          }}
+        />{" "}
+        <Layout className="site-layout">
+          <Sider
+            collapsible
+            collapsed={collapsed}
+            onCollapse={(value) => setCollapsed(value)}
+          >
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              defaultSelectedKeys={["1"]}
+              mode="inline"
+              items={items}
+            />
+          </Sider>
+          <Content
+            style={{
+              margin: "0 16px",
+            }}
+          >
+            <div
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                minHeight: 360,
+              }}
+            >
+              <Outlet />
+              <Chart />
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    </MainPage>
   );
 };
+
+const MainPage = styled.div``;
 
 export default Main;
