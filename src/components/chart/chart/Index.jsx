457a910e74c4af00e69ef199d2464625a035aec0
youbinpark <you8inpark@gmail.com>
2022-10-20 00:21:53 +0900

    Add: Chart 캡쳐& 저장 기능 추가

--- src/components/chart/chart/Index.jsx
+++ src/components/chart/chart/Index.jsx
@@ -1,26 +1,48 @@
 import Vistor from "../monthly-vistor/Index";
+import { Button } from "antd";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
 import AgeGroup from "../user-age-group/Index";
 import Post from "../monthly-post/Index";
 import styled from "styled-components";
+import { DownloadOutlined } from "@ant-design/icons";
 
 const Chart = () => {
+  const onSave = () => {
+    domtoimage.toBlob(document.getElementById("capture")).then((blob) => {
+      saveAs(blob, "chart.png");
+    });
+  };
+
   return (
-    <Charts>
-      <Vistor />
-      <AgeGroup />
-      <Post />
-    </Charts>
+    <>
+      <ImgSave type="primary" icon={<DownloadOutlined />} onClick={onSave}>
+        Image Save
+      </ImgSave>
+      <div id="capture">
+        <Charts>
+          <Vistor />
+          <AgeGroup />
+          <Post />
+        </Charts>
+      </div>
+    </>
   );
 };
 
 const Charts = styled.div`
   display: flex;
   justify-content: space-between;
+  width: calc(50% -10px);
 
   @media (max-width: 1280px) {
     flex-direction: column;
     width: 100%;
   }
+`;
+
+const ImgSave = styled(Button)`
+  margin-bottom: 10px;
 `;
 
 export default Chart;
